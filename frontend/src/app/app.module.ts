fa14f9a94ccc165e086fc4336d8acbcda8de52d9
uldahlalex <41024316+uldahlalex@users.noreply.github.com>
2023-05-03 12:19:49 +0200

    install ionic

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
